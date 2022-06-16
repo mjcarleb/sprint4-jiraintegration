0038fbabcbe2b04b19a99d878c03418898ffc069
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-06-16 07:00:09 -0400

    renaming job worker

---
+++ run_post_jira_worker.py
@@ -0,0 +1,43 @@
+import asyncio
+import uuid
+
+from pyzeebe import ZeebeWorker, create_insecure_channel
+import requests
+
+####################################################
+#      SETUP TO USE C8 ZEEBE
+####################################################
+channel = create_insecure_channel(
+    hostname='44.199.120.6',
+    port=26500
+)
+
+# Create single threaded worker
+worker = ZeebeWorker(channel)
+
+####################################################
+#          DEFINE WORKERS
+####################################################
+# Define work this client should do when trade_match_worker job exists in Zeebe
+@worker.task(task_type="rest_call")
+async def execute_rest_call(method, url):
+
+    print(f"method = {method}")
+    print(f"url = {url}")
+
+    if method == "get":
+        r = requests.get(url)
+    else:
+        breakpoint()
+
+    status = r.status_code
+
+    return {"webhook_uuid": f"webhook_{uuid.uuid4()}"}
+
+# Main loop
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(worker.work())
+finally:
+    loop.stop()
+    loop.close()
