acd5d75bdb17536880a7afbbbadd74018a3ea0da
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-06-21 08:07:04 -0400

    working

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,7 +1,12 @@
 from celery import Celery
+import sys
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def exit(x, y):
+    sys.exit(0)
