a9d082f6ee19993c91cb3987e1f39bc04f65ec9a
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-06-23 14:52:25 -0400

    working where I post to Jira

--- run_post_jira_worker.py
+++ run_post_jira_worker.py
@@ -1,5 +1,6 @@
 import asyncio
 import uuid
+import json
 
 from pyzeebe import ZeebeWorker, create_insecure_channel
 import requests
@@ -20,28 +21,37 @@ worker = ZeebeWorker(channel)
 ####################################################
 # Define work this client should do when trade_match_worker job exists in Zeebe
 @worker.task(task_type="jira_interface")
-async def execute_rest_call(method, url):
+async def execute_rest_call(method, url, project, issuetype, summary, assignee):
+    print(f"url = {url}")
+    print(f"method = {method}")
+    print(f"project = {project}")
+    print(f"issuetype = {issuetype}")
+    print(f"summary = {summary}")
+    print(f"assignee = {assignee}")
 
     webhook_uuid = f"webhook_{uuid.uuid4()}"
-    print(f"url = {url} (will be Jira)")
-    print(f"method = {method} (will be post with Jira)")
     print(f"webhook_uuid = {webhook_uuid}")
-    print()
 
-
-    # make post
-    # use Jira username and secret
-    # use Jira URL
     # include in the post the variables in form of following:
     #   - trade
-    #   - assignee
-    #   - webhook_uuid
-    if method == "get":
-        r = requests.get(url)
+    #   - webhook url (public flask url /CTL)
+    username = "mcarlebach@ctepl.com"  # Jira
+    password = "KNIubBqwWYTWbD7bmYVBDE66"  # Jir
+    payload = {
+        "fields": {"project":  {"key":  project},
+                   "issuetype":  {"name":  issuetype},
+                   "summary":  summary,
+                   "customfield_10298":  webhook_uuid
+                   }
+    }
+    if method == "post":
+        r = requests.post(url=url, auth=(username, password), json=payload)
     else:
         breakpoint()
 
     status = r.status_code
+    print(f"Jira response code = {status}")
+    print()
 
     return {"webhook_uuid": f"{webhook_uuid}"}
 
